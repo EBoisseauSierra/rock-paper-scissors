f097964c02b77ddac0744cf5a3ad28c5399b504b
Étienne Boisseau-Sierra <etienne.boisseau.sierra@gmail.com>
2018-06-18 17:18:38 -0400

    Refactor code to be triggered by button click

--- scripts/gui.js
+++ scripts/gui.js
@@ -1,78 +1,87 @@
-function computerPlay () {
-switch (Math.floor(Math.random() * 3)) {
-  case 0:
-  return 'rock';
-  case 1:
-  return 'paper';
-  case 2:
-  return 'scissors';
-  default:
-  console.error('Computer played something weird!');
-  return;
-}
+let playerSelection
+let computerSelection
+let scorePlayer = 0;
+let scoreComputer = 0;
+
+let btns = document.querySelectorAll('button');
+btns.forEach(btn => btn.addEventListener('click', playRound))
+
+function playRound(e) {
+    playerSelection = e.target.name;
+    computerSelection = computerPlay();
+    gameResult = defineWinner(playerSelection, computerSelection);
+    if (gameResult === 'player') increaseScorePlayer();
+    if (gameResult === 'computer') increaseScoreComputer();
 }
 
-function playRound (playerSelection, computerSelection) {
-playerSelection = playerSelection.toLowerCase();
-switch (playerSelection) {
-  case 'rock':
-  switch (computerSelection) {
-    case 'rock':
-    return 'Rock vs. rock: TIE!'
-    case 'paper':
-    return 'Paper covers rock: COMPUTER wins!'
-    case 'scissors':
-    return 'Rock crushes scissors: PLAYER wins!'
-  }
-  case 'paper':
-  switch (computerSelection) {
-    case 'rock':
-    return 'Paper covers rock: PLAYER wins!'
-    case 'paper':
-    return 'Paper vs. paper: TIE!'
-    case 'scissors':
-    return 'Scissors cut paper: COMPUTER wins!'
-  }
-  case 'scissors':
-  switch (computerSelection) {
-    case 'rock':
-    return 'Rock crushes scissors: COMPUTER wins!'
-    case 'paper':
-    return 'Scissors cut paper: PLAYER wins!'
-    case 'scissors':
-    return 'Scissors vs. scissors: TIE!'
-  }
-  default:
-  console.error(`Player played "${playerSelection}", which is not a valid play.`);
-  console.info('Enter either "paper", "rock", or "scissors"');
-  return playRound(prompt('Player, what is your choice?'),computerSelection);
-}
+function computerPlay() {
+    switch (Math.floor(Math.random() * 3)) {
+      case 0:
+        return 'rock';
+      case 1:
+        return 'paper';
+      case 2:
+        return 'scissors';
+      default:
+        console.error('Computer played something weird!');
+      return;
+    }
 }
 
-function game () {
-const nb_games = prompt("How many games you want to play?");
-console.info('To play, enter either "paper", "rock", or "scissors"');
-let playerSelection,
-computerSelection,
-scorePlayer = 0,
-scoreComputer = 0;
-
-for (i = 0; i < nb_games; i++) {
-  playerSelection = prompt('Player, what is your choice?');
-  computerSelection = computerPlay();
-  gameResult = playRound(playerSelection, computerSelection);
-  console.log(gameResult);
-  if (gameResult.includes('PLAYER')) scorePlayer++;
-  if (gameResult.includes('COMPUTER')) scoreComputer++;
+function defineWinner (playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    switch (playerSelection) {
+      case 'rock':
+          switch (computerSelection) {
+            case 'rock':
+                console.log('Rock vs. rock: TIE!');
+                return 'tie';
+            case 'paper':
+                console.log('Paper covers rock: COMPUTER wins!');
+                return 'computer';
+            case 'scissors':
+                console.log('Rock crushes scissors: PLAYER wins!');
+                return 'player';
+          }
+      case 'paper':
+          switch (computerSelection) {
+            case 'rock':
+                console.log('Paper covers rock: PLAYER wins!');
+                return 'player';
+            case 'paper':
+                console.log('Paper vs. paper: TIE!');
+                return 'tie';
+            case 'scissors':
+                console.log('Scissors cut paper: COMPUTER wins!');
+                return 'computer';
+          }
+      case 'scissors':
+          switch (computerSelection) {
+            case 'rock':
+                console.log('Rock crushes scissors: COMPUTER wins!');
+                return 'computer';
+            case 'paper':
+                console.log('Scissors cut paper: PLAYER wins!');
+                return 'player';
+            case 'scissors':
+                console.log('Scissors vs. scissors: TIE!');
+                return 'tie';
+          }
+      default:
+        console.error(`Player played "${playerSelection}", which is not a valid play.`);
+        console.info('Enter either "paper", "rock", or "scissors"');
+        return playRound(prompt('Player, what is your choice?'),computerSelection);
+    }
 }
 
-if (scorePlayer > scoreComputer) {
-  console.log(`PLAYER wins ${scorePlayer}:${scoreComputer}! Congratulation!`);
+function increaseScorePlayer() {
+    scorePlayer +=1;
+    let scoreText = document.querySelector('#score-player');
+    scoreText.textContent = scorePlayer.toString();
 }
-else if (scorePlayer < scoreComputer) {
-  console.log(`COMPUTER wins ${scoreComputer}:${scorePlayer}! JS is too strong…`);
-}
-else {
-  console.log(`You are tie: ${scorePlayer}:${scoreComputer}!`);
-}
+
+function increaseScoreComputer() {
+    scoreComputer +=1;
+    let scoreText = document.querySelector('#score-computer');
+    scoreText.textContent = scoreComputer.toString();
 }
